9696986ab18ec08d77f67dd77c7197bde802a403
Dmitry <lmahaon@yandex.ru>
2025-02-04 10:31:37 +0300

    Tune pagination

--- cats/pagination.py
+++ cats/pagination.py
@@ -1,5 +1,15 @@
 from rest_framework import pagination
+from rest_framework.response import Response
+from collections import OrderedDict
 
 
 class CatsPagination(pagination.PageNumberPagination):
     page_size = 20
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('cnt', self.page.paginator.count),
+            ('nxt', self.get_next_link()),
+            ('prev', self.get_previous_link()),
+            ('results', data)
+        ]))
